d7e33a538228ecfeb9e4b368a5edd7a3842a14e3
tdsparrow <sqallowlee@gmail.com>
2015-01-28 10:06:56 +0000

    parse login page

--- src/wechater_app.erl
+++ src/wechater_app.erl
@@ -10,6 +10,11 @@
 %% ===================================================================
 
 start(_StartType, _StartArgs) ->
+    httpc:set_options([{cookies, enabled}
+                      , {proxy, case os:getenv("http_proxy") of
+                                    false -> {undefined, []};
+                                    Proxy -> {Proxy, ["localhost"]}
+                                end}]),
     wechater_sup:start_link().
 
 stop(_State) ->
--- src/wechater_login_fsm.erl
+++ src/wechater_login_fsm.erl
@@ -1,7 +1,7 @@
 -module(wechater_login_fsm).
 -behaviour(gen_fsm).
 
--export([start_link/0, init/1, handle_info/2]).
+-export([start_link/0, init/1, handle_info/3]).
 
 -export([not_start/2, fetch_login_page/2]).
 
@@ -58,8 +58,107 @@ new_login(Profile) ->
     {next_state, fetch_login_page, NewProfile#wx_profile{timer = Timer}}.
     
 
-fetch_wx_main_page(Profile) ->
-    ok.
+fetch_wx_login_page(Profile = #wx_profile{requests = Requests}) ->
+    case lists:keyfind(main_page, 1, Requests) of
+        Id -> httpc:cancel_request(Id);
+        false -> ok
+    end,
+    {ok, RequestId} = httpc:request(get, {"https://wx.qq.com", []}, [], [{sync, false}]),
+    NewRequests = lists:keystore(main_page, 1, Requests, {main_page, RequestId}),
+    
+    Profile#wx_profile{requests = NewRequests}.
 
 fetch_wx_uuid(Appid, Profile) ->
     ok.
+
+poll_wx_login_status(Profile) ->
+    ok.
+
+handle_info({http, {RequestId, Result}}, fetch_login_page
+           , Profile = #wx_profile{requests = Requests}) ->
+    NewRequests = lists:keydelete(RequestId, 2, Requests),
+    NewProfile = Profile#wx_profile{requests = NewRequests},
+    case Result of
+        {ok, {_, _, B}} ->
+            fetch_login_page({get_uuid, parse_appid(B)}
+                            , NewProfile);
+        _ -> new_login(NewProfile)
+    end.
+
+parse_appid(Html) ->
+    ok.
+
+parse_loginjs(Html) ->
+    Tree = mochiweb_html:parse(Html),
+    {_, Attr, _} = tree_find(fun({N, A, C}) ->
+                                     case N of
+                                         <<"script">> ->
+                                             case lists:keyfind(<<"src">>, 1, A) of
+                                                 {K, V} ->
+                                                     case re:run(V, "login[0-9]+.js") of
+                                                         {match, _} ->
+                                                             true;
+                                                         _ -> false
+                                                     end;
+                                                 _ -> false
+                                             end;
+                                         _ -> false
+                                     end
+                             end, Tree),
+    {<<"src">>, Js} = lists:keyfind(<<"src">>, 1, Attr),
+    binary_to_list(Js).
+
+tree_find(Func, {N, A, []}) ->
+    case Func({N, A, []}) of
+        true ->
+            {N, A, []};
+        _ ->
+            not_found
+    end;
+
+tree_find(Func, Tree = {N, A, [C|Cs]}) ->
+    case Func(Tree) of
+        true ->
+            Tree;
+        _ ->
+            if is_tuple(C) ->
+                    case tree_find(Func, C) of
+                        not_found ->
+                            tree_find(Func, Cs);
+                        Node -> Node
+                    end;
+               true ->
+                    tree_find(Func, Cs)
+            end
+    end;
+tree_find(Func, {N,_}) ->
+    not_found;
+
+tree_find(Func, [N|Ns]) ->
+    if is_tuple(N) ->
+            case tree_find(Func, N) of
+                not_found ->
+                    tree_find(Func, Ns);
+                Node ->
+                    Node
+            end;
+       true ->
+            tree_find(Func, Ns)
+    end;
+
+tree_find(Func, []) ->
+    not_found.
+
+
+
+-ifdef(TEST).
+parse_appid_test() ->
+    {ok, Dir} = file:get_cwd(),
+    erlang:display(Dir),
+    {ok, Html} = file:read_file("../test/fixtures/login.html"),
+    ?assertEqual("wx782c26e4c19affb", parse_appid(Html)).
+
+parse_loginjs_test() ->
+    {ok, Html} = file:read_file("../test/fixtures/login.html"),
+    ?assertEqual("https://res.wx.qq.com/zh_CN/htmledition/js/login214065.js", parse_loginjs(Html)).
+-endif.
